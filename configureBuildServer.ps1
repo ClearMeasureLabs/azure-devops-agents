bb510f542935c70703779d9786fa183f8af493b9
Kyle Nunery <kyle.nunery@gmail.com>
2019-05-30 12:33:58 -0400

    make the build tool installer a proper PS function

--- configureBuildServer.ps1
+++ configureBuildServer.ps1
@@ -1,33 +1,46 @@
-Write-Host "Installing Chocolatey"
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+function Install-BuildTools {
+	param(
+		$DatabaseUserName,
+		$DatabasePassword,
+		$OrganizationUrl,
+		$BuildAgentPAT,
+		$AgentPool = "Private VS2019",
+		$NumOfAgents = 4
+	)
 
-$Packages = 'git',`
-			'visualstudiocode',`
-			'sql-server-express',`
-			'sql-server-management-studio'
-			
-Write-Host "Installing Packages"
-ForEach ($PackageName in $Packages)
-{ choco install $PackageName -y }
+	Write-Host "Installing Chocolatey"
+	Set-ExecutionPolicy Bypass -Scope Process -Force
+	Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+	
+	$Packages = 'git',`
+				'visualstudiocode',`
+				'sql-server-express',`
+				'sql-server-management-studio'
+				
+	Write-Host "Installing Packages"
+	ForEach ($PackageName in $Packages)
+	{ choco install $PackageName -y }
+	
+	# Write-Host "Downloading agent zip file"
+	# $url = "https://vstsagentpackage.azureedge.net/agent/2.150.3/vsts-agent-win-x64-2.150.3.zip"
+	# $out = "C:/temp/vsts-agent-win-x64-2.150.3.zip"
+	# Invoke-WebRequest -Uri $url -OutFile $out
+	
+	# $BuildAgentPAT = $Env:BuildAgentPAT
+	# $OrganizationUrl = $Env:OrganizationUrl
+	# $AgentPool = "Private VS2019"
+	
+	Write-Host "Installing $NumOfAgents build agents"
+	For ($i=1; $i -le $NumOfAgents; $i++) {
+		$AgentName = "$Env:ComputerName-$i"
+		Write-Host "Installing agent $AgentName"
+		choco install azure-pipelines-agent --params "'/Directory:c:\agents\agent$i /AgentName:$AgentName /Token:$BuildAgentPAT /Pool:$AgentPool /Url:$OrganizationUrl'" -fy
+	}
 
-Write-Host "Restarting computer"
-Restart-Computer
-
-# Write-Host "Downloading agent zip file"
-# $url = "https://vstsagentpackage.azureedge.net/agent/2.150.3/vsts-agent-win-x64-2.150.3.zip"
-# $out = "C:/temp/vsts-agent-win-x64-2.150.3.zip"
-# Invoke-WebRequest -Uri $url -OutFile $out
-
-$BuildAgentPAT = $Env:BuildAgentPAT
-$OrganizationUrl = $Env:OrganizationUrl
-$AgentPool = "Private VS2019"
-
-$NumOfAgents = 4
-For ($i=1; $i -le $NumOfAgents; $i++) {
-    $AgentName = "$Env:ComputerName-$i"
-	Write-Host "Installing agent $AgentName"
-	choco install azure-pipelines-agent --params "'/Directory:c:\agents\agent$i /AgentName:$AgentName /Token:$BuildAgentPAT /Pool:$AgentPool /Url:$OrganizationUrl'" -fy
+	# Write-Host "Restarting computer"
+	# Restart-Computer
 }
 
-#future: /Work:WorkDirectory set to temp drive for better performance
+# ideas
+#future: agents /Work:WorkDirectory set to temp drive for better performance
+# should agents machine drives be encrypted?
