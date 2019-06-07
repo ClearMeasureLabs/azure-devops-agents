1dbcdf7e29ce8368aced3e385e9aea7513cacb1b
Kyle Nunery <kyle.nunery@gmail.com>
2019-06-07 15:47:46 -0400

    remove unused files

--- deployBuildVM.ps1
+++
@@ -1,5 +0,0 @@
-$AdminPassword = ConvertTo-SecureString  $Env:AdminPassword -AsPlainText -Force
-
-
-#New-AzureRmResourceGroupDeployment -ResourceGroupName Test -TemplateFile ./template/template.json -TemplateParameterFile ./template/parameters.json -adminPassword (ConvertTo-SecureString -String [password here] -AsPlainText -Force)
-New-AzureRmResourceGroupDeployment -ResourceGroupName Test -TemplateFile ./template/template.json -TemplateParameterFile ./template/parameters.json -adminPassword $AdminPassword
